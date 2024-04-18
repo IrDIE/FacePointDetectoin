fix area calculation bug

utils/visualisation.py
@@ -32,7 +32,7 @@ def show_landmarks(file_path, imagename, label_ext ='.pts'):
         cv2.imshow('',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-def ced_plot(error_lists, save_path, title = '', thr_x = 10):
+def ced_plot(error_lists, save_path, data_name, title = '', thr_x = 0.08):
     os.makedirs(save_path, exist_ok=True)
     legend = []
     plt.figure(figsize=(10, 10), dpi=80)
@@ -41,8 +41,10 @@ def ced_plot(error_lists, save_path, title = '', thr_x = 10):
         color = (random(), random(), random())
         values, base = np.histogram(error_list, bins=len(error_list))
         cumulative = np.cumsum(values) / len(values)
+        print(cumulative)
+        print(base)
         plt.plot(base[:-1], cumulative, color=color)
-        area = np.round(np.trapz(cumulative, dx=0.001), 3)
+        area = np.round(np.trapz(y=cumulative[base[1:] <= thr_x], x=base[base <= thr_x][1:] ), 7)
         legend.append(mpatches.Patch(color=color, label=f'\nArea={area}'))
 
     plt.legend(handles=legend, fontsize=18)
@@ -51,8 +53,9 @@ def ced_plot(error_lists, save_path, title = '', thr_x = 10):
     plt.ylabel('Fraction of test imeges with error < CED ', fontsize=18)
     plt.xlabel('Normalised mean error', fontsize=18)
     plt.xlim([0, thr_x])
+    plt.ylim([0, 1])
 
-    plt.savefig(f'{save_path}ced.png')
+    plt.savefig(f'{save_path}ced_{data_name}.png')
     logger.info(f'CED plot saved to: {save_path}')
 
 